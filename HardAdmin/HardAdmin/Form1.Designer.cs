4064491d7ab33cc94c5f7e6f4a2fe370a1fd291a
matiasgon95 <matiasegonzalez95@gmail.com>
2026-08-31 18:42:08 -0300

    Cambio Usuario por Nombre de usuario

--- HardAdmin/HardAdmin/Form1.Designer.cs
+++ HardAdmin/HardAdmin/Form1.Designer.cs
@@ -41,11 +41,12 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(304, 61);
+            this.label1.Location = new System.Drawing.Point(249, 65);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.Size = new System.Drawing.Size(101, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Usuario:";
+            this.label1.Text = "Nombre de Usuario:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
