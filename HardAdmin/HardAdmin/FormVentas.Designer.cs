0a22c28c010d3e6ba3baf08a7c9a7c9d06d4c3a3
matiasgon95 <matiasegonzalez95@gmail.com>
2026-09-07 06:39:32 -0300

    Formulario de ventas en proceso

---
+++ HardAdmin/HardAdmin/FormVentas.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace HardAdmin
+{
+    partial class FormVentas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.pnlTitulo = new System.Windows.Forms.Panel();
+            this.pnlFiltros = new System.Windows.Forms.Panel();
+            this.pnlInferior = new System.Windows.Forms.Panel();
+            this.dgvVentas = new System.Windows.Forms.DataGridView();
+            this.gbFiltros = new System.Windows.Forms.GroupBox();
+            this.dtpDesde = new System.Windows.Forms.DateTimePicker();
+            this.dtpHasta = new System.Windows.Forms.DateTimePicker();
+            this.lblBuscar = new System.Windows.Forms.Label();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.lblVendedor = new System.Windows.Forms.Label();
+            this.cmbVendedores = new System.Windows.Forms.ComboBox();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.colId_venta = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colFecha = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colNroFactura = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colCliente = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colVendedor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colMedioPago = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colTotal = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pnlFiltros.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVentas)).BeginInit();
+            this.gbFiltros.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlTitulo
+            // 
+            this.pnlTitulo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTitulo.Location = new System.Drawing.Point(0, 0);
+            this.pnlTitulo.Name = "pnlTitulo";
+            this.pnlTitulo.Size = new System.Drawing.Size(800, 50);
+            this.pnlTitulo.TabIndex = 0;
+            // 
+            // pnlFiltros
+            // 
+            this.pnlFiltros.Controls.Add(this.gbFiltros);
+            this.pnlFiltros.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlFiltros.Location = new System.Drawing.Point(0, 50);
+            this.pnlFiltros.Name = "pnlFiltros";
+            this.pnlFiltros.Size = new System.Drawing.Size(800, 70);
+            this.pnlFiltros.TabIndex = 1;
+            // 
+            // pnlInferior
+            // 
+            this.pnlInferior.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlInferior.Location = new System.Drawing.Point(0, 385);
+            this.pnlInferior.Name = "pnlInferior";
+            this.pnlInferior.Size = new System.Drawing.Size(800, 65);
+            this.pnlInferior.TabIndex = 2;
+            // 
+            // dgvVentas
+            // 
+            this.dgvVentas.AllowUserToAddRows = false;
+            this.dgvVentas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvVentas.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colId_venta,
+            this.colFecha,
+            this.colNroFactura,
+            this.colCliente,
+            this.colVendedor,
+            this.colMedioPago,
+            this.colTotal});
+            this.dgvVentas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvVentas.Location = new System.Drawing.Point(0, 120);
+            this.dgvVentas.Name = "dgvVentas";
+            this.dgvVentas.ReadOnly = true;
+            this.dgvVentas.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvVentas.Size = new System.Drawing.Size(800, 265);
+            this.dgvVentas.TabIndex = 3;
+            // 
+            // gbFiltros
+            // 
+            this.gbFiltros.Controls.Add(this.lblFecha);
+            this.gbFiltros.Controls.Add(this.btnLimpiar);
+            this.gbFiltros.Controls.Add(this.cmbVendedores);
+            this.gbFiltros.Controls.Add(this.lblVendedor);
+            this.gbFiltros.Controls.Add(this.txtBuscar);
+            this.gbFiltros.Controls.Add(this.lblBuscar);
+            this.gbFiltros.Controls.Add(this.dtpHasta);
+            this.gbFiltros.Controls.Add(this.dtpDesde);
+            this.gbFiltros.Location = new System.Drawing.Point(0, 0);
+            this.gbFiltros.Name = "gbFiltros";
+            this.gbFiltros.Size = new System.Drawing.Size(800, 70);
+            this.gbFiltros.TabIndex = 0;
+            this.gbFiltros.TabStop = false;
+            this.gbFiltros.Text = "Filtros de búsqueda";
+            // 
+            // dtpDesde
+            // 
+            this.dtpDesde.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpDesde.Location = new System.Drawing.Point(56, 24);
+            this.dtpDesde.Name = "dtpDesde";
+            this.dtpDesde.Size = new System.Drawing.Size(76, 22);
+            this.dtpDesde.TabIndex = 0;
+            // 
+            // dtpHasta
+            // 
+            this.dtpHasta.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpHasta.Location = new System.Drawing.Point(138, 24);
+            this.dtpHasta.Name = "dtpHasta";
+            this.dtpHasta.Size = new System.Drawing.Size(78, 22);
+            this.dtpHasta.TabIndex = 1;
+            // 
+            // lblBuscar
+            // 
+            this.lblBuscar.AutoSize = true;
+            this.lblBuscar.Location = new System.Drawing.Point(244, 32);
+            this.lblBuscar.Name = "lblBuscar";
+            this.lblBuscar.Size = new System.Drawing.Size(44, 13);
+            this.lblBuscar.TabIndex = 2;
+            this.lblBuscar.Text = "Buscar:";
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(294, 24);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(129, 22);
+            this.txtBuscar.TabIndex = 3;
+            // 
+            // lblVendedor
+            // 
+            this.lblVendedor.AutoSize = true;
+            this.lblVendedor.Location = new System.Drawing.Point(456, 33);
+            this.lblVendedor.Name = "lblVendedor";
+            this.lblVendedor.Size = new System.Drawing.Size(60, 13);
+            this.lblVendedor.TabIndex = 4;
+            this.lblVendedor.Text = "Vendedor:";
+            // 
+            // cmbVendedores
+            // 
+            this.cmbVendedores.FormattingEnabled = true;
+            this.cmbVendedores.Location = new System.Drawing.Point(522, 25);
+            this.cmbVendedores.Name = "cmbVendedores";
+            this.cmbVendedores.Size = new System.Drawing.Size(121, 21);
+            this.cmbVendedores.TabIndex = 5;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(682, 47);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(118, 23);
+            this.btnLimpiar.TabIndex = 6;
+            this.btnLimpiar.Text = "Limpiar Filtros";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Location = new System.Drawing.Point(12, 33);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(40, 13);
+            this.lblFecha.TabIndex = 7;
+            this.lblFecha.Text = "Fecha:";
+            // 
+            // colId_venta
+            // 
+            this.colId_venta.HeaderText = "id_venta";
+            this.colId_venta.Name = "colId_venta";
+            this.colId_venta.ReadOnly = true;
+            this.colId_venta.Visible = false;
+            // 
+            // colFecha
+            // 
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.colFecha.DefaultCellStyle = dataGridViewCellStyle1;
+            this.colFecha.HeaderText = "Fecha";
+            this.colFecha.Name = "colFecha";
+            this.colFecha.ReadOnly = true;
+            // 
+            // colNroFactura
+            // 
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.colNroFactura.DefaultCellStyle = dataGridViewCellStyle2;
+            this.colNroFactura.HeaderText = "Nro de Factura";
+            this.colNroFactura.Name = "colNroFactura";
+            this.colNroFactura.ReadOnly = true;
+            // 
+            // colCliente
+            // 
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.colCliente.DefaultCellStyle = dataGridViewCellStyle3;
+            this.colCliente.HeaderText = "Cliente";
+            this.colCliente.Name = "colCliente";
+            this.colCliente.ReadOnly = true;
+            // 
+            // colVendedor
+            // 
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.colVendedor.DefaultCellStyle = dataGridViewCellStyle4;
+            this.colVendedor.HeaderText = "Vendedor";
+            this.colVendedor.Name = "colVendedor";
+            this.colVendedor.ReadOnly = true;
+            // 
+            // colMedioPago
+            // 
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.colMedioPago.DefaultCellStyle = dataGridViewCellStyle5;
+            this.colMedioPago.HeaderText = "Medio de Pago";
+            this.colMedioPago.Name = "colMedioPago";
+            this.colMedioPago.ReadOnly = true;
+            // 
+            // colTotal
+            // 
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.colTotal.DefaultCellStyle = dataGridViewCellStyle6;
+            this.colTotal.HeaderText = "Total";
+            this.colTotal.Name = "colTotal";
+            this.colTotal.ReadOnly = true;
+            // 
+            // FormVentas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvVentas);
+            this.Controls.Add(this.pnlInferior);
+            this.Controls.Add(this.pnlFiltros);
+            this.Controls.Add(this.pnlTitulo);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "FormVentas";
+            this.Text = "FormVentas";
+            this.pnlFiltros.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVentas)).EndInit();
+            this.gbFiltros.ResumeLayout(false);
+            this.gbFiltros.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlTitulo;
+        private System.Windows.Forms.Panel pnlFiltros;
+        private System.Windows.Forms.Panel pnlInferior;
+        private System.Windows.Forms.DataGridView dgvVentas;
+        private System.Windows.Forms.GroupBox gbFiltros;
+        private System.Windows.Forms.DateTimePicker dtpDesde;
+        private System.Windows.Forms.DateTimePicker dtpHasta;
+        private System.Windows.Forms.Label lblBuscar;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.Label lblVendedor;
+        private System.Windows.Forms.ComboBox cmbVendedores;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colId_venta;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colFecha;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colNroFactura;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colCliente;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colVendedor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colMedioPago;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colTotal;
+    }
+}
